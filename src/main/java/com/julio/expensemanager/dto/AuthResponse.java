189750ba7153908dc4f7522853fe5c49d1d67a99
Julio Cesar <jcesar206@hotmail.com>
2026-03-05 09:24:42 -0600

    Se sube archivo AuthResponse para responder a solicitudes de registro y login de la app

---
+++ src/main/java/com/julio/expensemanager/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.julio.expensemanager.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthResponse {
+	private String token;
+	private String refreshToken;
+}
